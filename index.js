9cb2b5ee903b1bfc23b2b262189d39d94fdd1a5b
Marcelo Yugo <marcelo.tonossu@engineeringhub.man>
2024-06-05 23:26:37 +0100

    Initial commit

--- index.js
+++ index.js
@@ -12,5 +12,10 @@ app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
 });
 
+const userRouter = require('./routes/user.router')
+
+app.use("/api", userRouter)
+
+app.listen(process.env.PORT, () => console.log("Server is running on port 5000"))
 // Export the Express API
 module.exports = app
