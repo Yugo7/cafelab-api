da80dec56fdfd19e1275fd896dcb63956a4e3279
Marcelo Yugo <marcelo.tonossu@engineeringhub.man>
2024-06-06 00:45:10 +0100

    Initial commit

--- index.js
+++ index.js
@@ -1,23 +1,16 @@
 import express from 'express';
-import { createClient } from '@supabase/supabase-js';
+import productsRouter from './routes/products.router';
 
 const app = express();
 
 // enable JSON body parser
 app.use(express.json());
 
-// Create a single supabase client for interacting with your database
-const supabaseUrl = 'https://sbkrffeyngcjbzrwhvdq.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNia3JmZmV5bmdjamJ6cndodmRxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTMxOTM2MjgsImV4cCI6MjAyODc2OTYyOH0.COR1kdIkfK19CRDIrdwmI2CQD8VXdnF46cc0Ql8ofyU';
-const supabase = createClient(supabaseUrl, supabaseKey);
+// Use products router
+app.use('/products', productsRouter);
 
 app.get('/', async (req, res) => {
-    let { data, error } = await supabase
-        .from('products')
-        .select('*')
-
-    if (error) return res.status(500).json({ error: error.message });
-    return res.status(200).json(data);
+    res.send('Hello World!');
 });
 
 export default app;
---
+++ routes/products.router.js
@@ -0,0 +1,49 @@
+import express from 'express';
+import { createClient } from '@supabase/supabase-js';
+
+const router = express.Router();
+
+// Create a single supabase client for interacting with your database
+const supabaseUrl = 'https://sbkrffeyngcjbzrwhvdq.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNia3JmZmV5bmdjamJ6cndodmRxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTMxOTM2MjgsImV4cCI6MjAyODc2OTYyOH0.COR1kdIkfK19CRDIrdwmI2CQD8VXdnF46cc0Ql8ofyU';
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+// Create
+router.post('/', async (req, res) => {
+    const { data, error } = await supabase
+        .from('products')
+        .insert([req.body]);
+    if (error) return res.status(500).json({ error: error.message });
+    return res.status(200).json(data);
+});
+
+// Read
+router.get('/', async (req, res) => {
+    const { data, error } = await supabase
+        .from('products')
+        .select('*');
+    if (error) return res.status(500).json({ error: error.message });
+    return res.status(200).json(data);
+});
+
+// Update
+router.put('/:id', async (req, res) => {
+    const { data, error } = await supabase
+        .from('products')
+        .update(req.body)
+        .eq('id', req.params.id);
+    if (error) return res.status(500).json({ error: error.message });
+    return res.status(200).json(data);
+});
+
+// Delete
+router.delete('/:id', async (req, res) => {
+    const { data, error } = await supabase
+        .from('products')
+        .delete()
+        .eq('id', req.params.id);
+    if (error) return res.status(500).json({ error: error.message });
+    return res.status(200).json(data);
+});
+
+export default router;
