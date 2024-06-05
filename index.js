507ca35e82b5d00182e80f01c992c8f43f6d318f
Marcelo Yugo <marcelo.tonossu@engineeringhub.man>
2024-06-06 00:36:15 +0100

    Initial commit

--- index.js
+++ index.js
@@ -7,13 +7,13 @@ const app = express();
 app.use(express.json());
 
 // Create a single supabase client for interacting with your database
-const supabaseUrl = 'your-supabase-url';
-const supabaseKey = 'your-supabase-key';
+const supabaseUrl = 'https://sbkrffeyngcjbzrwhvdq.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNia3JmZmV5bmdjamJ6cndodmRxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTMxOTM2MjgsImV4cCI6MjAyODc2OTYyOH0.COR1kdIkfK19CRDIrdwmI2CQD8VXdnF46cc0Ql8ofyU';
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 app.get('/', async (req, res) => {
     let { data, error } = await supabase
-        .from('your-table')
+        .from('products')
         .select('*')
 
     if (error) return res.status(500).json({ error: error.message });
